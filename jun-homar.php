ec23461a4abb15a8a9b73713b3e3a2cad473c874
jun722 <s09366js@sfc.keio.ac.jp>
2012-08-01 20:56:09 +0900

    Update jun-homar.php

--- jun-homar.php
+++ jun-homar.php
@@ -1,7 +1,16 @@
 <div id="jh-1" class="step test" data-x="-5000" data-y="0" data-scale="2">
-  <p>This is sample for aa.</p>
-  <ul><li>hoge</li><li>hoge</li></ul>
+  <p>Tutorial</p>
+  <ul><li>Startup</li><li>Tips</li><li>TO/GA Settings</li></ul>
 </div>
 <div id="jh-2" class="step test" data-x="-3000" data-y="0" data-scale="2">
-  <p>This is 2nd sample for aa.</p>
+  <p>Startup</p>
+  <ul><li>新しいプロジェクトを作る</li><li>プロジェクト名を決める</li><li>TO/GAに入る</li><li>フォルダーを開く</li><li>新しいフォルダーを作る</li><li>フォルダー名を付ける</li><li>新しいファイルを作る</li><li>ファイル名を付ける</li><li>編集画面を開く</li><li>コードを書く</li><li>実行画面を表示する</li><li>TO/GAへようこそ</li></ul>
+</div>
+<div id="jh-3" class="step test" data-x="-1000" data-y="0" data-scale="2">
+  <p>Tips</p>
+  <ul><li>ファイルを削除する</li><li>ファイルの名前を変える</li></ul>
+</div>
+<div id="jh-4" class="step test" data-x="1000" data-y="0" data-scale="2">
+  <p>TOGA/Settings</p>
+  <ul><li>データベースの情報を入力</li><li>サーバーの情報を入力</li><li>メールの設定</li><li>ハッシュ関数を入力</li></ul>
 </div>
